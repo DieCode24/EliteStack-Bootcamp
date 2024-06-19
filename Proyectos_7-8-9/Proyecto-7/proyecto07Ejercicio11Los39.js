ac385cd499e09167c1d45a0ff912af2ba8ce66c8
DieCode24 <diego.giraldo.0424@gmail.com>
2024-06-19 13:23:11 -0500

    Update details

--- Proyectos_7-8-9/Proyecto-7/proyecto07Ejercicio11Los39.js
+++ Proyectos_7-8-9/Proyecto-7/proyecto07Ejercicio11Los39.js
@@ -1,6 +1,6 @@
 // - Fecha de publicación: [19/06/2024]
 // - Número de la tarea: [11]
-// - Hora: [12:00]PM
+// - HoraData: [12:00]PM
 // - Versión de la tarea: [01]
 // - Autores: [Todos los ingenieros del grupo (39)]
 // - Nombre del lenguaje utilizado: [Javascript]
@@ -11,25 +11,24 @@
 // --- | Programa de Ingeniería de Sistemas y Computación |---
 
 // - Descripción:
-// Programa que a partir de horas minutos y segundos calcula el total de milisegundos
+// Programa que a partir de horas, minutos y segundos calcula el total de milisegundos
 
 
 //npm install readline-sync
-const readlineSync = require('readline-sync');
+const readLineSync = require('readline-sync');
 
+console.log("# Programa que a partir de horas, minutos y segundos calcula el total de milisegundos\n\n")    // Titulo del programa (UX - Amable con el usuario)
 
-console.log("por favor digite el numero de horas, minutos y segundos que desea convertir a milisegundos: ");//solicita los valores
+console.log("Usuario, por favor digite el numero de hora, minutos y segundos que desea convertir a milisegundos\n\n"); // Solicita los valores
 
-let hora = readlineSync.question("Digite horas: ");//recibe los valores
-let minutos = readlineSync.question("Digite minutos: ");
-let segundos = readlineSync.question("Digite segundos: ");
+let horaData = readLineSync.question("- Digite hora => "); //Recibe el valor para la variable hora
+let minutosData = readLineSync.question("\n- Digite minutos => "); //Recibe el valor para la variable minutos
+let segundosData = readLineSync.question("\n- Digite segundos => "); //Recibe el valor para la variable segundos
 
-hora=parseFloat(hora);//convierte a flotantes
-minutos=parseFloat(minutos);
-segundos=parseFloat(segundos);
+horaData = parseFloat(horaData);    // Convierte a flotantes el valor registrado en la variable hora
+minutosData = parseFloat(minutosData);    // Convierte a flotantes el valor registrado en la variable minutos
+segundosData = parseFloat(segundosData);    // Convierte a flotantes el valor registrado en la variable segundos
 
-let milisegundos=(((hora*60)*60)*1000)+((minutos*60)*1000)+(segundos *1000);//calcula los milisegundos
+let milisegundosData = (((horaData * 60) * 60)*1000)+((minutosData * 60) * 1000)+(segundosData * 1000); // Calcula los milisegundos teniendo en cuenta los valorews registrados
 
-console.log(`La cantidad de milisegundos pora ${hora} hora, ${minutos} minutos y ${segundos} segundos es  de ${milisegundos} milisegundos`);//imprime los milisegundos
-
-
+console.log(`\n\nLa cantidad de milisegundos por ${horaData} horaData, ${minutosData} minutos y ${segundosData} segundos es de:\n=> ${milisegundosData} milisegundos`);    // Imprime el resultado
